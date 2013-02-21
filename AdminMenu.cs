800cbda9b8e9ca491012a7c0a01a720f14ef6bfe
Yhnavein Calzie <admin@puredev.eu>
2013-02-21 20:45:21 +0100

    init commit with sample module

---
+++ AdminMenu.cs
@@ -0,0 +1,30 @@
+﻿using Orchard.UI.Navigation;
+using Orchard.Localization;
+using Orchard.Environment.Extensions;
+
+namespace BootstrapAdminSample
+{
+    [OrchardFeature("BootstrapAdminSample")]
+    public class AdminMenu : INavigationProvider
+    {
+        public Localizer T { get; set; }
+
+        public AdminMenu()
+        {
+            T = NullLocalizer.Instance;
+        }
+        private const string AreaName = "BootstrapAdminSample";
+
+        public string MenuName { get { return "admin"; } }
+
+        public void GetNavigation(NavigationBuilder builder)
+        {
+            builder.AddImageSet("bootstrap")
+                .Add(T("Bootstrap"), "5",
+                    menu => menu
+                        .Add(T("Bootstrap Sample"), "0",
+                            item => item.Action("Index", "Sample", new { area = AreaName })));
+
+        }
+    }
+}
---
+++ Controllers/SampleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Orchard.Themes;
+using Orchard.UI.Admin;
+
+namespace BootstrapAdminSample.Controllers
+{
+    [Admin]
+    [Themed]
+    [Authorize]
+    public class SampleController : Controller
+    {
+        //
+        // GET: /Sample/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
